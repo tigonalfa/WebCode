bb932b049c7959c953d4f7ef516280de0277d483
tigonalfa <62378365+tigonalfa@users.noreply.github.com>
2023-08-24 14:04:31 +0530

    Add files via upload

---
+++ webCode/A2.2/script.js
@@ -0,0 +1,288 @@
+let nationFlagsData = [
+    {
+      id: 1,
+      img: "pictures/canada.jpg",
+      count_english: "Canada",
+      count_hindi: "कनाडा"
+    },
+    {
+      id: 2,
+      img: "pictures/china.jpg",
+      count_english: "China",
+      count_hindi: "चीन"
+    },
+    {
+      id: 3,
+      img: "pictures/czech.jpg",
+      count_english: "czech",
+      count_hindi: "चेकोस्लोवाकिया"
+    },
+    {
+      id: 4,
+      img: "pictures/egypt.jpg",
+      count_english: "egypt",
+      count_hindi: "मिस्र"
+    },
+    {
+      id: 5,
+      img: "pictures/england.jpg",
+      count_english: "england",
+      count_hindi: "इंगलैंड"
+    },
+    {
+      id: 6,
+      img: "pictures/france.jpg",
+      count_english: "france",
+      count_hindi: "फ्रांस"
+    },
+    {
+      id: 7,
+      img: "pictures/germany.jpg",
+      count_english: "germany",
+      count_hindi: "जर्मन फेड प्रतिनिधि"
+    },
+    {
+      id: 8,
+      img: "pictures/indon.jpg",
+      count_english: "indonaisa",
+      count_hindi: "इंडोनेशिया"
+    },
+    {
+      id: 9,
+      img: "pictures/iran.jpg",
+      count_english: "iran",
+      count_hindi: "ईरान"
+    },
+    {
+      id: 10,
+      img: "pictures/iraq.jpg",
+      count_english: "iraq",
+      count_hindi: "इराक"
+    },
+    {
+      id: 11,
+      img: "pictures/israel.jpg",
+      count_english: "israel",
+      count_hindi: "इजराइल"
+    },
+    {
+      id: 12,
+      img: "pictures/italy.jpg",
+      count_english: "italy",
+      count_hindi: "इटली"
+    },
+    {
+      id: 13,
+      img: "pictures/japan.jpg",
+      count_english: "japan",
+      count_hindi: "जापान"
+    },
+    {
+      id: 14,
+      img: "pictures/kuwait.jpg",
+      count_english: "kuwait",
+      count_hindi: "कुवैट"
+    },
+    {
+      id: 15,
+      img: "pictures/malaysia.jpg",
+      count_english: "malaysia",
+      count_hindi: "मलेशिया"
+    },
+    {
+      id: 16,
+      img: "pictures/maldives.jpg",
+      count_english: "maldives",
+      count_hindi: "मालदीव"
+    },
+    {
+      id: 17,
+      img: "pictures/mauritius.jpg",
+      count_english: "mauritius",
+      count_hindi: "मॉरीशस"
+    },
+    {
+      id: 18,
+      img: "pictures/myanmar.jpg",
+      count_english: "myanmar",
+      count_hindi: "म्यांमार"
+    },
+    {
+      id: 19,
+      img: "pictures/netherlands.jpg",
+      count_english: "netherlands",
+      count_hindi: "नीदरलैंड"
+    },
+    {
+      id: 20,
+      img: "pictures/pakistan.jpg",
+      count_english: "pakistan",
+      count_hindi: "पाकिस्तान"
+    },
+    {
+      id: 21,
+      img: "pictures/philipines.jpg",
+      count_english: "philipines",
+      count_hindi: "फिलिपींस"
+    },
+    {
+      id: 22,
+      img: "pictures/russia.jpg",
+      count_english: "russia",
+      count_hindi: "रूस"
+    },
+    {
+      id: 23,
+      img: "pictures/saudiArabia.jpg",
+      count_english: "saudi arabia",
+      count_hindi: "सऊदी अरब"
+    },
+    {
+      id: 24,
+      img: "pictures/southAfrica.jpg",
+      count_english: "south africa",
+      count_hindi: "दक्षिण अफ्रीका"
+    },
+    {
+      id: 25,
+      img: "pictures/southKorea.jpg",
+      count_english: "south korea",
+      count_hindi: "दक्षिण कोरिया"
+    },
+    {
+      id: 26,
+      img: "pictures/sriLanka.jpg",
+      count_english: "sri lanka",
+      count_hindi: "श्रीलंका"
+    },
+    {
+      id: 27,
+      img: "pictures/switxerland.jpg",
+      count_english: "switzerland",
+      count_hindi: "स्विट्ज़रलैंड"
+    },
+    {
+      id: 28,
+      img: "pictures/thailand.jpg",
+      count_english: "thailand",
+      count_hindi: "थाईलैंड"
+    },
+    {
+      id: 29,
+      img: "pictures/unitedArabEmerates.jpg",
+      count_english: "united arab emirates",
+      count_hindi: "संयुक्त राज्य अमेरिका"
+    },
+    {
+      id: 30,
+      img: "pictures/unitedKingdom.jpg",
+      count_english: "united kingdom",
+      count_hindi: "यूनाइटेड किंगडम"
+    },
+    {
+      id: 31,
+      img: "pictures/usa.jpg",
+      count_english: "usa",
+      count_hindi: "संयुक्त राज्य अमेरिका"
+    },
+    {
+      id: 32,
+      img: "pictures/worldMap.jpg",
+      count_english: "world map",
+      count_hindi: "दुनिया का नक्शा"
+    },
+    {
+      id: 33,
+      img: "pictures/YUGO.jpg",
+      count_english: "YUGO",
+      count_hindi: "यूगोस्लाविया"
+    },
+    {
+      id: 34,
+      img: "pictures/india.jpg",
+      count_english: "India",
+      count_hindi: "भारत"
+    },
+    {
+      id: 35,
+      img: "pictures/uno.jpg",
+      count_english: "U.N.O.",
+      count_hindi: "संयुक्त राष्ट्र संघ"
+    },
+    {
+      id: 36,
+      img: "pictures/australia.jpg",
+      count_english: "Australia",
+      count_hindi: "ऑस्ट्रेलिया"
+    },
+    {
+      id: 37,
+      img: "pictures/bangladesh.jpg",
+      count_english: "Bangladesh",
+      count_hindi: "बांग्लादेश"
+    },
+    {
+      id: 38,
+      img: "pictures/bhutan.jpg",
+      count_english: "Bhutan",
+      count_hindi: "भूटान"
+    },
+    {
+      id: 39,
+      img: "pictures/nepal.jpg",
+      count_english: "Nepal",
+      count_hindi: "नेपाल"
+    },
+    {
+      id: 40,
+      img: "pictures/newZealand.jpg",
+      count_english: "New Zealand",
+      count_hindi: "न्यूज़ीलैंड"
+    }
+  ];
+
+let gallery = document.getElementById("gallery");
+//  ES6 function
+
+
+let generateGallery = () => {
+    return (gallery.innerHTML = nationFlagsData.map((x) => {
+        return `
+        
+         <div class="flex-cell">
+            <img src = ${x.img} alt="Image 1">
+           <div class = "countries" id="INDIA">
+           <p> ${x.count_english}</p>
+           <p> ${x.count_hindi} </p>
+           </div>
+         </div>
+
+        
+
+    
+        
+        
+        `
+    })
+    .join(""));
+        
+    
+    
+};
+
+generateGallery()
+
+
+
+
+
+
+    //` <div class="flex-container">
+    // <div class="flex-cell">
+    // <img src="pictures/india.jpg" alt="Image 1">
+    // <div class="countries" id="INDIA">
+    //   <p>INDIA</p>
+    //   <p>भारत </p>
+    // </div>
+    
+    // `
