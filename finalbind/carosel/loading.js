e9d8b6f24c8cc1e1c11ec3dc56db5b4ac533b650
tigonalfa <62378365+tigonalfa@users.noreply.github.com>
2023-09-09 09:21:48 +0530

    Add files via upload

---
+++ finalbind/carosel/loading.js
@@ -0,0 +1,293 @@
+
+
+let a3_data = [
+    {
+      id: 1,
+      name: "Aarushi Kakkar",
+      indexLink: "https://akak506.github.io/coding_with_web/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 2,
+      name: "Aditya Banujit Narayan",
+      indexLink: "https://banjothepaladin.github.io/codingforweb_/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 3,
+      name: "Aisha Neha",
+      indexLink: "https://winterquartet.github.io/coding_for_web/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 4,
+      name: "Ananya Chandorkar",
+      indexLink: "https://ananyachandorkar.github.io/Coding-for-Web/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 5,
+      name: "Ananya Rajhans",
+      indexLink: "https://ananyarajhans.github.io/Coding_for_Web/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 6,
+      name: "Ananya Sarangi",
+      indexLink: "https://ananya-sarangi.github.io/classwork/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 7,
+      name: "Annie Koshy Vaithara",
+      indexLink: "https://reinakv.github.io/Studio-CodingforWeb/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 8,
+      name: "Anurag Sunil Shinde",
+      indexLink: "https://garunaanurag.github.io/Codingfortheweb/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 9,
+      name: "Catherine Thomas",
+      indexLink: "https://cathy-th.github.io/classwork/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 10,
+      name: "G K Ashwath",
+      indexLink: "https://huntarex.github.io/coursepage/index.htmll",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 11,
+      name: "Manya Mankad",
+      indexLink: "https://mia16zang.github.io/classwork/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 12,
+      name: "Mehal Kumar",
+      indexLink: "https://mehalkumar.github.io/CodingforWeb.mk/CodingforWeb/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 13,
+      name: "Mrinalini Kumar",
+      indexLink: "https://mk134340.github.io/Coding-for-Web/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 14,
+      name: "Pulkit Kishore",
+      indexLink: "https://pulkishore.github.io/Coding_4_Web/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 15,
+      name: "Raghav Srivatsan Sivakumar",
+      indexLink: "https://nyxtaku.github.io/Coding-for-Web-Assignments/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 16,
+      name: "Rahul Ferreira",
+      indexLink: "https://rahul8x-web.github.io/index/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 17,
+      name: "Sahasrabudhe Kanteya Keyur",
+      indexLink: "https://kanteya.github.io/coding-for-web/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 18,
+      name: "Sameeksha Bijuraj Kodappully",
+      indexLink: "https://samkjshxkwjdx.github.io/coding4web/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 19,
+      name: "Sharanya Chandrashekar",
+      indexLink: "https://sharanyacs13.github.io/Coding-For-Web-/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 20,
+      name: "Shiva Bheri",
+      indexLink: "https://supercewl.github.io/Coding_For_Web/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 21,
+      name: "Soumil Kumar",
+      indexLink: "https://kingnoob5146.github.io/coding4web/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 22,
+      name: "Sumedha Nair",
+      indexLink: "https://sumedha1009.github.io/codingforweb/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 23,
+      name: "Supragyay Kaval",
+      indexLink: "https://tigonalfa.github.io/WebCode/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      id: 24,
+      name: "Varun Padmanabhan",
+      indexLink: "https://varunpaddy.github.io/codingforweb/",
+      profilePic: "bird1.png",
+      profileDes: "abcdefghijklmnopqrstuvwxyz"
+    }
+  ]
+
+
+let swiper_wrapper = document.getElementById("swiper_wrapper")
+
+let generateGallery = () => {
+    return (swiper_wrapper.innerHTML = a3_data.map((x) => {
+    return `<div class="swiper-slide" id = "${x.id}">
+                <div class="popup-container">
+                <a href="../profile_a3/profile.html">
+                <img class="profilepic" src="Assets/profile.jpg" alt=""></a>
+                    <h2>${x.name}</h2>
+                    <p>${x.profileDes}</p>
+                    <p></p>
+            
+                </div>
+            </div>
+    `})
+    .join (""));
+
+};
+
+generateGallery();
+
+function hanf(item){
+  const itemId = item.id;
+  window.location.href = `../profile_a3/profile.html?id=${itemId}`
+}
+
+const carouselItem = document.querySelectorAll('.swiper-slide')
+carouselItem.forEach(item => {
+  item.addEventListener('click', () => {
+    hanf(item);
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("loading-screen").style.display = "none";
+});
+
+const popupButton = document.getElementById("popup-button");
+const popupContainers = document.querySelectorAll(".popup-container");
+const prevButton = document.getElementsByClassName("swiper-button-prev");
+const nextButton = document.getElementsByClassName("swiper-button-next");
+
+popupButton.addEventListener("click", () => {
+    // Toggle the display of popup containers
+    popupContainers.forEach(container => {
+        container.style.display = "block"
+    });
+});
+
+// Add a click event listener to the document
+document.addEventListener("click", (event) => {
+    // Check if the click target is outside of any of the popup containers
+    let clickedOutside = true;
+    popupContainers.forEach(container => {
+        if (container.contains(event.target) || event.target.id === "popup-button" || event.target.id === "swiper-button-prev" || event.target.id === "swiper-button-next") {
+            // Clicked inside the current container or on one of the buttons
+            clickedOutside = false;
+        }
+    });
+
+    if (clickedOutside) {
+        // Close all pop-up containers
+        popupContainers.forEach(container => {
+            container.style.display = "none";
+        });
+    }
+});
+
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal', // Change to 'horizontal'
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});
